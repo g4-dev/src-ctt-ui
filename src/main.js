318e158a1d9b059b2c3fe08fe9d9a0a5811c8be3
loic-roux <69loloro10@gmail.com>
2020-06-11 22:04:59 +0200

    try branching api

--- src/main.js
+++ src/main.js
@@ -13,14 +13,7 @@ new Vue({
   store,
   vuetify,
   beforeMount() {
-    api.session.interceptors.response.use(
-      response => response,
-      error => {
-        this.$store.dispatch('error/handleXHR', error.response)
-        return Promise.reject(error)
-      }
-    )
-    console.log(api.get("/transcripts"))
+    console.log(api.get('/transcripts'))
   },
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,25 @@
+const path = require('path')
 module.exports = {
   transpileDependencies: ['vuetify'],
   publicPath: process.env.NODE_ENV === 'production' ? '/ctt-ui/' : '/',
-  // css: {
-  //   loaderOptions: {
-  //     css: {
-  //       modules: {
-  //         localIdentName: '[name]-[hash]'
-  //       },
-  //       localsConvention: 'camelCaseOnly'
-  //     }
-  //   }
-  // }
+  css: {
+    requireModuleExtension: true,
+    loaderOptions: {
+      css: {
+        modules: {
+          localIdentName: '[name]-[hash]'
+        },
+        localsConvention: 'camelCaseOnly'
+      }
+    }
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, './src/assets/sass/main.scss')]
+    }
+  },
+  devServer: {
+    proxy: 'http://localhost:8080'
+  }
 }
